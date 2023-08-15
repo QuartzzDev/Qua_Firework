c79ba108403923e9da8f5c60f3e6a70ba5057e0d
QuartzzDev <69876083+QuartzzDev@users.noreply.github.com>
2023-08-15 04:05:42 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -8,7 +8,7 @@ RegisterCommand('qua', function(source, args, rawCommand)
     -- İstediğiniz havaifişek koordinatları buraya ekleyebilirsiniz
     local fireworks = {
         {x = 100.0, y = 100.0, z = 100.0},
-        {x = 110.0, y = 110.0, z = 110.0},
+        {x = 1100.0, y = 1100.0, z = 11000.0},
         
     }
     
